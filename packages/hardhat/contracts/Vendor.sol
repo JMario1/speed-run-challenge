03baaf8092ca604039f7d836520a483b2d875787
JMario1 <jdonmarie@gmail.com>
2022-04-03 13:52:03 +0100

    Third challenge

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -6,18 +6,33 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
 
   YourToken public yourToken;
+  uint256 public constant tokensPerEth = 100;
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable {
+    uint tokens = msg.value * tokensPerEth;
+    yourToken.transfer(msg.sender, tokens);
+    emit BuyTokens(msg.sender, msg.value, tokens);
+
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public onlyOwner {
+    payable(msg.sender).transfer(address(this).balance);
+  } 
 
   // ToDo: create a sellTokens() function:
-
+  function sellTokens(uint256 amount) public {
+    yourToken.transferFrom(msg.sender, address(this), amount);
+    payable(msg.sender).transfer(amount/tokensPerEth);
+    emit SellTokens(msg.sender, amount, amount/tokensPerEth);
+  }
 }
